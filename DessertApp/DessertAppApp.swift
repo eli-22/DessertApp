4b10456c34b5d32bd576144d97261a97d9605d0f
eli-22 <109761870+eli-22@users.noreply.github.com>
2023-04-15 12:24:06 -0400

    Initial Commit

---
+++ DessertApp/DessertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertAppApp.swift
+//  DessertApp
+//
+//  Created by Elise on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
